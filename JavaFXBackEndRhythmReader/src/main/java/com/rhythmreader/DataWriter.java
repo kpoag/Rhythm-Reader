943f085140e2a787f182ed6f06fc5576979c1171
CSE-kmack0 <kriskam321@gmail.com>
2025-02-28 13:06:49 -0500

    fixed DataWriter.java SaveUser method #11 and SaveSongs method #19

---
+++ JavaFXBackEndRhythmReader/src/main/java/com/rhythmreader/DataWriter.java
@@ -0,0 +1,15 @@
+package com.rhythmreader;
+
+public class DataWriter {
+    public void doStuff() {
+
+    }
+
+    public static boolean saveUsers() {
+        return true;
+    }
+
+    public static boolean saveSongs() {
+        return false;
+    }
+}
