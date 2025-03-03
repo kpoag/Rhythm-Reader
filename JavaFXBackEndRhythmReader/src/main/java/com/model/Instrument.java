472ecf9a44330c516618d16aa05a34a16c4c5f76
Rondasha Bonds <rondashabonds@gmail.com>
2025-03-03 14:35:06 -0500

    updated class stubs

--- JavaFXBackEndRhythmReader/src/main/java/com/model/Instrument.java
+++ JavaFXBackEndRhythmReader/src/main/java/com/model/Instrument.java
@@ -1,5 +1,64 @@
 package com.model;
 
-public class Instrument {
-    
+public class Instrument { 
+    private String instrumentName;
+    private String songName;
+    private int tempo;
+    private double accuracyScore;
+
+    public Instrument(String instrumentName, String songName, int tempo, double accuracyScore) {
+        this.instrumentName = instrumentName;
+        this.songName = songName;
+        this.tempo = tempo;
+        this.accuracyScore = accuracyScore;
+    }
+
+    public String getInstrumentName() {
+        return instrumentName;
+    }
+
+    public void setInstrumentName(String instrumentName) {
+        this.instrumentName = instrumentName;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
+
+    public double getAccuracyScore() {
+        return accuracyScore;
+    }
+
+    public void setAccuracyScore(double accuracyScore) {
+        this.accuracyScore = accuracyScore;
+    }
+
+    public void playExercise() {
+        System.out.println(instrumentName + " is playing " + songName + " at " + tempo + " BPM.");
+    }
+
+    public void adjustTempo(int newTempo) {
+        this.tempo = newTempo;
+        System.out.println("Tempo adjusted to " + tempo + " BPM.");
+    }
+
+    public void evaluatePerformance(double newScore) {
+        this.accuracyScore = newScore;
+        System.out.println("Performance evaluated. New accuracy score: " + accuracyScore);
+    }
 }
+
+    
+
--- JavaFXBackEndRhythmReader/src/main/java/com/model/PreTest.java
+++ JavaFXBackEndRhythmReader/src/main/java/com/model/PreTest.java
@@ -1,5 +1,58 @@
 package com.model;
 
-public class PreTest {
-    
+import java.util.List;
+
+public class Pretest {
+    private List<String> questions;
+    private List<String> answers;
+    private String level;
+    private int score;
+
+    public Pretest(List<String> questions, List<String> answers, String level, int score) {
+        this.questions = questions;
+        this.answers = answers;
+        this.level = level;
+        this.score = score;
+    }
+
+    public List<String> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<String> questions) {
+        this.questions = questions;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void takeQuiz() {
+        System.out.println("Taking quiz at " + level + " level.");
+    }
+
+    public void evaluateScore(int newScore) {
+        this.score = newScore;
+        System.out.println("Quiz evaluated. New score: " + score);
+    }
 }
