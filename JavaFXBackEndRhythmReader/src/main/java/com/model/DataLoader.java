11aa797a03f01b54929a037a1a6e153f2e22a8d6
Jaylen Graham <jaylengraham95@gmail.com>
2025-03-03 20:27:43 -0500

    DataLoader updated

---
+++ JavaFXBackEndRhythmReader/src/main/java/com/model/DataLoader.java
@@ -0,0 +1,23 @@
+package com.model;
+import java.util.ArrayList;
+
+
+
+public class DataLoader {
+    public static ArrayList<User> getUsers()
+    {
+        return new ArrayList<User>();
+    }
+
+    public static ArrayList<Song> getSongs()
+    {
+        return new ArrayList<Song>();
+    }
+}
+
+
+
+
+
+
+
