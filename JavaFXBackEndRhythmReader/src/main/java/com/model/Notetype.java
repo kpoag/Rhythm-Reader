8a938de0e38fee50ea87bbc1b09145473695b1d7
Rondasha Bonds <rondashabonds@gmail.com>
2025-03-05 14:52:45 -0500

    updated class stubs

--- JavaFXBackEndRhythmReader/src/main/java/com/model/Notetype.java
+++ JavaFXBackEndRhythmReader/src/main/java/com/model/Notetype.java
@@ -1,5 +1,13 @@
 package com.model;
 
-public class Notetype {
+public enum Notetype { 
+ SIXTEENTH_NOTE,
+ EIGHTH_NOTE,
+ QUARTER_NOTE,
+ HALF_NOTE,
+ WHOLE_NOTE,
+ WHOLE_REST,
+ HALF_REST,
+ QUARTER_REST;
     
 }
