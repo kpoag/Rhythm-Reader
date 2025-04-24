31995520c0dc35cba4d584fc1393d4d23e1c837d
CSE-kmack0 <kriskam321@gmail.com>
2025-04-24 11:48:25 -0400

    committing to pull

--- JavaFXBackEndRhythmReader/src/main/java/com/controllers/profileController.java
+++ JavaFXBackEndRhythmReader/src/main/java/com/controllers/profileController.java
@@ -3,15 +3,32 @@ package com.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class profileController implements Initializable {
 
+    @FXML
+    private AnchorPane launch_pane;
 
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Button editProfileButton;
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("profileController initialized!");
+    }
+
+    @FXML
+    private void handleEditProfile() {
+        System.out.println("Profile clicked");
     }
     
 }
