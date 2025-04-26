ee10cf9596601e0e8c4774baf0f95758a7241a93
CSE-kmack0 <kriskam321@gmail.com>
2025-04-26 19:24:36 -0400

    Updating profile and settings controller

--- JavaFXBackEndRhythmReader/src/main/java/com/controllers/profileController.java
+++ JavaFXBackEndRhythmReader/src/main/java/com/controllers/profileController.java
@@ -1,34 +1,75 @@
 package com.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
 
+import com.model.RRFacade;
+import com.model.User;
+import com.rhythmreader.App;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
-public class profileController implements Initializable {
-
-    @FXML
-    private AnchorPane launch_pane;
+public class profileController {
 
     @FXML
-    private Label nameLabel;
+    private Text editProfile;
 
     @FXML
     private Button editProfileButton;
 
+    @FXML
+    private Text emailAddress;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("profileController initialized!");
+    @FXML
+    private Text favorites;
+
+    @FXML
+    private Button favoritesButton;
+
+    @FXML
+    private Text firstName;
+
+    @FXML
+    private Text lastName;
+
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private Text settings;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    private Text username;
+
+    @FXML
+    void switchToEditProfile(ActionEvent event) {
+       // App.navigateTo(editProfile.fxml);
     }
 
     @FXML
-    private void handleEditProfile() {
-        System.out.println("Profile clicked");
+    void switchToFavorites(ActionEvent event) {
+        // App.navigateTo(favorites.fxml);
     }
-    
+
+    @FXML
+    void settingsButtonClicked(MouseEvent event) throws IOException {
+        App.navigateTo("settings");
+    }
+
+    @FXML
+    private void initialize() {
+        RRFacade facade = RRFacade.getInstance();
+        User currUser = facade.getCurrentUser();
+        username.setText(currUser.getUserName());
+        firstName.setText(currUser.getFirstName());
+        lastName.setText(currUser.getLastName());
+    }
+
 }
